55896a16a3a86bb723f45bcfbe32719e65fc0da6
Yhoo2 <yhooi2011@gmail.com>
2024-01-07 11:36:05 +0300

    dz 1 OS v2

--- bashSkript1.sh
+++ bashSkript1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-for (( i=0; i<20; i++))
+if [ !-d "./attachments" ]; then
+ mkdir ./attachments/
+ fi
+for (( i=0; i<20; i++ ))
 do
-curl https://picsum.photos/800/400 -L > ./attachments/file_name$i
+ curl https://picsum.photos/800/400 -L > ./attachments/file_name$i
 done
