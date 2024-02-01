e71fa1a0ff4a15f618eec3ab4eb40493df928c19
Yhooi2 <yhooi2011@gmail.com>
2024-02-01 11:55:16 +0300

    dz1

---
+++ python/dz1.py
@@ -0,0 +1,7 @@
+print('Input type of pet:')
+type_of_pet = input()
+print('Input age pet:')
+age = input()
+print('name of pet:')
+name = input()
+print( 'This is', type_of_pet, 'is name', name, 'and age is', age)  
---
+++ python/dz1_2.py
@@ -0,0 +1,7 @@
+print('Input type of Homo:')
+homo_habilis = input()
+homo_erectus = input()
+homo_neanderthalensis = input()
+homo_sapiens = input()
+print(homo_habilis, homo_erectus, homo_neanderthalensis, homo_sapiens, sep=' => ') 
+
