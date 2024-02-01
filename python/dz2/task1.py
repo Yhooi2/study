227430c3f0dc54fa1a1425e302f3a0374b332dfc
Yhooi2 <yhooi2011@gmail.com>
2024-02-01 13:48:28 +0300

    dz2

---
+++ python/dz2/task1.py
@@ -0,0 +1,4 @@
+print('Input side of the rectangle:')
+a, b = map(int, input().split())
+print('The area of the rectangle is', a * b)
+print('The rectangle perimeter is', (a + b) * 2)
---
+++ python/dz2/task2.py
@@ -0,0 +1,2 @@
+a = input()
+print(int(a[3]) ** int(a[4]) * int(a[2]) / (int(a[0]) - int(a[1])))
