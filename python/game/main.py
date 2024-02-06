07e511b6c0d705fc80d1b410c5032bf4bd998d21
Yhoo2 <yhooi2011@gmail.com>
2024-02-06 19:04:52 +0300

    tick

--- python/game/main.py
+++ python/game/main.py
@@ -2,22 +2,19 @@ from map import Map
 import time 
 import os
 
-TICK_SLEEP = 0.05
+TICK_SLEEP = 1 
 
 tmp = Map(20, 10)
-tmp.generate_forest(5, 10)
+tmp.generate_forest(6, 10)
 for i in range(3):
     tmp.generate_river(10)
 tmp.print_map()
-
+print()
 tick = 1
 
 while True:
-    try:
-        os.system("clear")
-    except Exception:
-        os.system("cls")
-    print("TICK", tick)
+    os.system("cls") # clear
     tmp.print_map()
+    print()
     tick += 1
     time.sleep(TICK_SLEEP)
