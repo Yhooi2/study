80a32a384bf7929c95680d9a73eb2bbadcd42c79
Yhoo2 <yhooi2011@gmail.com>
2024-02-06 13:06:44 +0300

    game1

--- python/game/map.py
+++ python/game/map.py
@@ -1,13 +1,14 @@
-CELL_TYPES ="\U0001F7E9\U0001F332\%&*"
+CELL_TYPES = "🟩🌲🌊🏥🏪"
 class Map:
     def print_map(self):
-        print('== ' * (self.w + 2))
+        print('⬛' * (self.w + 2))
         for  row in self.cells:
-            print('||', end=' ')
+            print('⬛', end='')
             for cell in row:
-                print(CELL_TYPES[cell], end=' ')
-            print('||')
-        print('== ' * (self.w + 2), end=' ')
+                if cell >= 0 and cell < len(CELL_TYPES):
+                    print(CELL_TYPES[cell], end='')
+            print('⬛')
+        print('⬛' * (self.w + 2), end='')
 
     def check_bounds(self, x, y):
         if (x < 0 or y < 0 or x >= self.h or y >= self.w):
@@ -23,5 +24,9 @@ class Map:
         self.h = h
         self.cells = [[0 for i in range(w)] for j in range(h)]
 tmp = Map(10, 10)
-tmp.cells[1][1] = 4
+
+tmp.cells[1][1] = 1
+tmp.cells[2][2] = 2
+tmp.cells[3][3] = 3
+tmp.cells[4][4] = 4
 tmp.print_map()
