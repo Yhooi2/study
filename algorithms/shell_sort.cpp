78389a141178d26c94c46d5d4e5cf198a42b97a8
Yhooi2 <yhooi2011@gmail.com>
2024-02-10 19:09:33 +0300

    s

--- algorithms/shell_sort.cpp
+++ algorithms/shell_sort.cpp
@@ -1,14 +1,39 @@
 #include<ctime>
+#include<iostream>
+#include <cstdlib>
 
-time_t start = clock();
-    class_one.function1();
+int SIZE = 5000000;
+
+int main() {
+    
+    int arr[SIZE] = {};
+
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand();
+    }
+
+    time_t start = clock();
+    int step;
+    int n = SIZE;
+    while (n != 1) {
+        n = n / 2;
+	for (int i = n; i < SIZE; ++i) {
+	    step = i - n;
+            if (arr[step] > arr[i]) {
+		std::swap(arr[i], arr[step]);
+		for (int j = step - n; j > -1; j -=n) {
+	            if (arr[j] > arr[step]) {
+			    std::swap(arr[j], arr[step]);
+			    step = j;
+	            } else break;
+		}
+	    }
+	}
+    }
 
 
+    time_t finish = clock();
 
-
-
-
- time_t finish = clock();
-
- double duration = (double)(finish - start)/ CLOCKS_PER_SEC;
- printf( "%2.1f seconds\n" duration );
+    double duration = (double)(finish - start)/ CLOCKS_PER_SEC;
+    std::cout << duration; 
+}
