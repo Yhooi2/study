9178213831e9f9851149036ec51f141e944c1d81
Yhooi2 <yhooi2011@gmail.com>
2024-02-12 00:40:43 +0300

    bed

--- algorithms/advanced_sorts/merge_sort.py
+++ algorithms/advanced_sorts/merge_sort.py
@@ -3,16 +3,15 @@ import timeit
 
 code_to_test = """
 import random
-import array
 import sys
 sys.setrecursionlimit(100000)
 
-n = 10 
-arr = array.array('i',[0]) * n
+
+n = 500000
+arr = list()
 for i in range(n):
     number = random.randint(1, 100)
-    arr.append(number)
-
+    arr.append( number )
 #print('Not sorted:')
 #print(arr)
 #print('------')
@@ -22,38 +21,34 @@ for i in range(n):
 def merge( arrLeftSort, arrRightSort ):
     lenLeft = len( arrLeftSort )
     lenRight = len( arrRightSort )
-    arrFull = array.array( 'i', [ 0 ] ) * ( lenLeft + lenRight )
-    index = 0
+    arrFull = list()
     j = 0
-    for i in range(lenRight):
+    for i in range( lenRight ):
         while j <  lenLeft and arrLeftSort[ j ] <= arrRightSort[ i ]:
-            arrFull[ index ] = arrLeftSort[ j ]
-            index += 1
+            arrFull.append( arrLeftSort[ j ] )
             j += 1
-        arrFull[ index ] = arrRightSort[ i ]
-        index += 1
+        arrFull.append( arrRightSort[ i ] )
+    arrFull.extend( arrLeftSort[ j: ] ) 
     return arrFull    
 
 def mergeSort( arr ):
-    size = len( arr )
+    size = len( arr ) 
     if size < 2:
         return arr
     sizeHalf = size // 2
 
-    arrLeft = array.array( 'i', [ 0 ] ) * sizeHalf
+    arrLeft = list()
     for i in range( sizeHalf ):
-        arrLeft[ i ] = arr[ i ]
+        arrLeft.append( arr[ i ] )
 
-    arrRight = array.array( 'i', [ 0 ] ) * ( size - sizeHalf )
+    arrRight = list()
     for i in range( sizeHalf, size ):
-        arrRight[ i - sizeHalf ] = arr[ i ]
-        
+        arrRight.append( arr[ i ] )
+
     arrLeftSort = mergeSort( arrLeft )
     arrRightSort = mergeSort( arrRight )
     
     return merge( arrLeftSort, arrRightSort )
-
-print( mergeSort( arr ) )
 
 #################################################
 #    print('sorted:')
