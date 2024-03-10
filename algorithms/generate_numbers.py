a653545a3e117fbd427922cbd2990b4818c52ea9
Yhooi2 <yhooi2011@gmail.com>
2024-03-10 15:40:55 +0300

    generate_numbers.py

---
+++ algorithms/generate_numbers.py
@@ -0,0 +1,13 @@
+
+def generate_numbers(base, size, prefix = ""):
+    """gererate all combinations of numbers in 
+       a given n-numeric system(base <= 10)"""`
+    if size == 0:
+        print(prefix)
+        return
+    for n in range(base):
+        generate_numbers(base, size-1, prefix + str(n))
+
+base, size = map(int, input().split())
+generate_numbers(base, size)
+
