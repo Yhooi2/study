19bcf87df293cae0328d690a236ec8072f477c99
Yhooi2 <yhooi2011@gmail.com>
2024-03-15 21:31:17 +0300

    correct

--- python/dz2/task1.py
+++ python/dz2/task1.py
@@ -1,4 +1,4 @@
 print('Input side of the rectangle:')
-a, b = map(int, input().split())
+a, b = map(float, input().split())
 print('The area of the rectangle is', a * b)
 print('The rectangle perimeter is', (a + b) * 2)
