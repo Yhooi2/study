f268a4b87fc9c0f6aa26f27c57a5cdd8c1525eef
Yhooi2 <yhooi2011@gmail.com>
2024-03-16 12:50:01 +0300

    correct dz11 task1

--- python/dz11/task1.py
+++ python/dz11/task1.py
@@ -2,10 +2,12 @@ my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16]
 
 def rec(n):
     if n < 0:
+        print("Конец списка")
         return
+    index = len(my_list) - n - 1
+    print(my_list[index], end=" ")
     rec(n - 1)
-    print(my_list[n], end=' ')
 
 size = len(my_list) - 1
 rec(size)
-print("Конец списка")
+print()
