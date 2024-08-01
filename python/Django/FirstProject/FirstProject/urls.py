5fc7e9874baeb47de49cdbe903f82ab8f6cfab64
Yhooi2 <yhooi2011@gmail.com>
2024-08-01 20:51:15 +0300

    lesson2.1

--- python/Django/FirstProject/FirstProject/urls.py
+++ python/Django/FirstProject/FirstProject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', include('News.urls')), # add this line to include News app's urls.py
+    path('blog/', include('Blog.urls')), # add this line to include Blog app's urls
 ]
