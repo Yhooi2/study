65c4f47b35eb4e14f1b3e85a36f2864d35ac83e4
Yhooi2 <yhooi2011@gmail.com>
2024-08-10 17:38:20 +0300

    papaginate

--- python/Django/NewsProject/News/templatetags/news_tags.py
+++ python/Django/NewsProject/News/templatetags/news_tags.py
@@ -1,6 +1,6 @@
 from News.models import Category
 from django import template
-from django.db.models import Count
+from django.db.models import Count, F
 register = template.Library()
 
 @register.simple_tag(name='get_list_categories')
@@ -11,5 +11,6 @@ def get_categories():
 @register.inclusion_tag('News/list_categories.html')
 def show_categories(arg1='Category', arg2='list'):
     # categories = Category.objects.all()
-    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    # categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
     return { 'arg1': arg1, 'arg2': arg2, 'categories': categories }
--- python/Django/NewsProject/News/views.py
+++ python/Django/NewsProject/News/views.py
@@ -4,9 +4,18 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 from .utils import MyMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .models import News, Category
 from .forms import NewsForm
+
+# def test(request):
+#     objects = ['john', 'doe', 'rick', 'jane', 'bill', 'sara', 'emma', 'sarah']
+#     paginator = Paginator(objects, 2)
+#     page_num = request.GET.get('page', 1)
+#     page_objects = paginator.get_page(page_num)
+#     return render(request, 'News/test.html', {'page_obj': page_objects})
+
 
 # def index(request):
     # news = News.objects.all()
@@ -21,6 +30,7 @@ class HomeNews(ListView, MyMixin):
     context_object_name = 'news'
     template_name = 'News/index.html'
     extra_context = {'title': 'Главная'}
+    paginate_by = 3
     
     def get_context_data(self, *,  object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -48,6 +58,7 @@ class NewsByCategory(ListView, MyMixin):
     template_name = 'News/index.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 3
     
     def get_context_date(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
