fe10eea49637c5f5a703d8977ddd6586d4ea41f7
Yhooi2 <yhooi2011@gmail.com>
2024-10-23 16:09:13 +0300

    diothant.py

---
+++ math/diothant.py
@@ -0,0 +1,26 @@
+def gcd_history(a, b):
+    if a < b:
+        a, b = b, a
+    quotients = []
+
+    while b != 0:
+        quotients.append(a // b)
+        a, b = b, a % b
+
+    return quotients
+
+def split_unit(a, b):
+    quotients = gcd_history(a, b)[-2::-1]
+    x, y = 0, 1
+    print(quotients)
+    for q in quotients:
+        x, y = y, x - q*y
+    return (x, y) if a > b else (y, x)
+
+a, b = map(int, input().split())
+x, y = split_unit(a,b)
+print(f"Еслм x = {x} и у = {y}, то {a}x + {b}у = {a*x + b * y}")
+
+        
+
+
