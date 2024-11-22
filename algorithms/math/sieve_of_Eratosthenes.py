709153d5adbf3ef2bfffda6a631d0e23fa41eff2
Art <artyhoo@icloud.com>
2024-11-22 11:31:49 +0300

    sieve_of_Eratosthenes_with_multi

--- algorithms/math/sieve_of_Eratosthenes.py
+++ algorithms/math/sieve_of_Eratosthenes.py
@@ -1,13 +1,14 @@
 
 def sieveOfEratoshenes(n):
-    sieve = [1 for _ in range(n+1)]
+    sieve = [[] for _ in range(n+1)]
+
     for i in range(2, n+1):
+        sieve[i].append(str(i))
+
         for j in range(i*2, n+1, i):
-            sieve[j] = 0
-        print(sieve)
+            sieve[j].append(str(i))
     
     for i in range(1, n+1):
-        if sieve[i]:
-            print(i)
+        print(",".join(sieve[i]))
 
 sieveOfEratoshenes(int(input()))
