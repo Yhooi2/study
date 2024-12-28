892b6eeeb170dd89284a8588ba44d7c2b55b553d
Art <artyhoo@icloud.com>
2024-12-28 12:41:28 +0300

    lesson5 math

---
+++ java/myFirstClass/src/Class5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Class5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double pow = scanner.nextDouble();
+        System.out.println(Math.pow(a, pow));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.abs(a - pow));
+        System.out.println(Math.max(a, pow));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.random()); // [0, 1)
+        System.out.println(((int) Math.random()) * 6 + 1); // (1, 6)
+
+    }
+}
