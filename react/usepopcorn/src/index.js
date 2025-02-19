3f5722a54fc7699f10d6a6d7fa591ce853c807cf
Art <artyhoo@icloud.com>
2025-02-19 23:05:11 +0300

    separate component stars

--- react/usepopcorn/src/index.js
+++ react/usepopcorn/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import StarsRaiting from "./StarsRaiting";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarsRaiting messages={["Terrible", "Bad", "Okey", "Good", "Amazing"]} />
+    <StarsRaiting size={24} color="red" defaultRating={3} />
+    {/* <App /> */}
   </React.StrictMode>
 );
