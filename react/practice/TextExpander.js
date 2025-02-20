a210adcdb6019c201b830eca0f1df08775f1ff1c
Art <artyhoo@icloud.com>
2025-02-20 11:59:13 +0300

    practice spoler

---
+++ react/practice/TextExpander.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
+
+function printText(text, wordsLimit) {
+  const words = text.split(" ");
+
+  if (words.length < wordsLimit) return text;
+  return words.slice(0, wordsLimit).join(" ") + "...";
+}
+function TextExpander({
+  children,
+  className = "",
+  expandButtonText = "Show",
+  buttonColor = "red",
+  collapseButtonText = "Collapse",
+  collapsedNumWords = 0,
+  expanded = false,
+}) {
+  const [show, setShow] = useState(expanded);
+  const buttonStyle = {
+    color: buttonColor,
+  };
+
+  return (
+    <div className={className}>
+      <p>{show ? children : printText(children, collapsedNumWords)}</p>
+      <button style={buttonStyle} onClick={() => setShow((show) => !show)}>
+        {show ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
+}
