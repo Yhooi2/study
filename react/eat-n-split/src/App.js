730202adc98764dcf52306eb935af2c5ae3d87e8
Art <artyhoo@icloud.com>
2025-02-25 11:38:55 +0300

    fix eat-n-split

--- react/eat-n-split/src/App.js
+++ react/eat-n-split/src/App.js
@@ -67,6 +67,7 @@ export default function App() {
         <FormSplitBill
           selectFriend={selectFriend}
           onSplitBill={handleSplitBill}
+          key={selectFriend.id}
         />
       )}
     </div>
