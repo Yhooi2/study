d3123aea778c7a540e10b5eaeae9d97f720008f6
Art <artyhoo@icloud.com>
2025-04-15 22:07:50 +0300

    useActionData(), Object.keys(obj).length, return errors

--- react/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ react/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -36,6 +36,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
@@ -55,6 +56,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {!!formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -93,6 +95,13 @@ export async function action({ request }) {
     priority: data.priority === "on",
     cart: JSON.parse(data.cart),
   };
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
+  }
+  if (Object.keys(errors).length > 0) return errors;
+
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
