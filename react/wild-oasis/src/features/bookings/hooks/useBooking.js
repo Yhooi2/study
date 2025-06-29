eff8f1bd9a48b589954b663d064ecb18ced38627
Art <artyhoo@icloud.com>
2025-06-29 22:42:31 +0300

    feat: add check-in system components and routing

--- react/wild-oasis/src/features/bookings/hooks/useBooking.js
+++ react/wild-oasis/src/features/bookings/hooks/useBooking.js
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBooking } from "../../../services/apiBookings";
+import { useParams } from "react-router-dom";
 
-function useBooking(bookingId) {
-  return useQuery({
+function useBooking() {
+  const { bookingId } = useParams();
+  const { data: booking, isLoading } = useQuery({
     queryKey: ["booking", bookingId],
     queryFn: () => getBooking(bookingId),
   });
+
+  return { booking, isLoading };
 }
 
 export default useBooking;
