4719ec5b95417a46d75dc938a3de7c975e492e28
Art <artyhoo@icloud.com>
2025-07-14 15:09:26 +0300

    feat(cabins): add CabinCard component and update about/cabins pages\n\n- Added app/cabins/CabinCard.js for displaying cabin info\n- Updated about/page.js and cabins/page.js placeholder, quality, aspect-square, object-cover fill

--- react/NEXTJS/wildhaven-website/app/cabins/page.js
+++ react/NEXTJS/wildhaven-website/app/cabins/page.js
@@ -1,9 +1,29 @@
+import CabinCard from "./CabinCard";
+
 export const metadata = {
   title: "Cabins",
 };
 
 async function Page() {
-  return <h2>Cabins</h2>;
+  const cabins = [];
+  return (
+    <div>
+      <h1 className="text-center text-4xl text-accent-400 font-medium mb-5">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-lg text-primary-200 mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
 }
 
 export default Page;
