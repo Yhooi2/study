1f79ab4b60a264abb2e3b0cf504dd5dc99b086ad
Art <artyhoo@icloud.com>
2025-07-16 14:40:23 +0300

    feat(cabins): add TextExpander component and improve cabin page layout
    - Added TextExpander component for expandable text functionality
    - Updated cabin detail page to use TextExpander for description

--- react/NEXTJS/wildhaven-website/app/cabins/[cabinId]/page.js
+++ react/NEXTJS/wildhaven-website/app/cabins/[cabinId]/page.js
@@ -2,6 +2,7 @@ import { getCabin, getCabins } from "@/app/_lib/data-service";
 
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import TextExpander from "@/app/_components/TextExpander";
 
 const styles = {
   li: "flex items-center gap-3",
@@ -9,7 +10,8 @@ const styles = {
 };
 
 export async function generateMetadata({ params }) {
-  const cabin = await getCabin(params.cabinId);
+  const { cabinId } = await params;
+  const cabin = await getCabin(cabinId);
   return {
     title: `${cabin.name} - Cabin`,
     description: cabin.description,
@@ -22,7 +24,8 @@ export async function generateStaticParams() {
 }
 
 async function Page({ params }) {
-  const cabin = await getCabin(params.cabinId);
+  const { cabinId } = await params;
+  const cabin = await getCabin(cabinId);
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 text-lg mb-24 py-3 p-10 border border-l-0 border-primary-800">
@@ -37,7 +40,9 @@ async function Page({ params }) {
         </div>
         <div>
           <h3 className="text-7xl text-accent-100 -translate-x-64 w-[150%] p-6 pb-1 bg-primary-950">{`Cabin ${cabin.name}`}</h3>
-          <p className="text-primary-300 mb-10">{cabin.description}</p>
+          <p className="text-primary-300 mb-10">
+            <TextExpander>{cabin.description}</TextExpander>
+          </p>
           <ul className="flex flex-col gap-4">
             <li className={styles.li}>
               <UsersIcon className={styles.icon} />
