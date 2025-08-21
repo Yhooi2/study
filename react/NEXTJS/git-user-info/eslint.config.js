37f8b34a5244dd9e44fb15171b9f026019a4cda8
Art <artyhoo@icloud.com>
2025-08-21 15:09:20 +0300

    pretter-eslint

---
+++ react/NEXTJS/git-user-info/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tailwind from "eslint-plugin-tailwindcss"
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+  ...tailwind.configs["flat/recommended"],
+  prettier.configs.recommended
+])
